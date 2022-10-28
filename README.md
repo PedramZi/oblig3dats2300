e54c2e00f9a167cfd97fd5854347e80a68acac0a
PedramZi <91465878+PedramZi@users.noreply.github.com>
2022-10-28 21:15:14 +0200

    readme

--- README.md
+++ README.md
@@ -7,9 +7,31 @@ Oppgaven er levert av følgende student:
 
 # Oppgavebeskrivelse
 
+NB! på øvingstimen ventet jeg en og halv time for å fikse ting med gradle og hadde litt problem med build structure men 
+fikk ikke mye hjelp i timen men ble bedt om å forklare situasjonen fordi at det har noe med rettingen å gjøre.
+
 I oppgave 1 så gikk jeg frem ved å initialisere roten som r, z ble satt null og komper som en komperator som er
 en hjelpevariabel. deretter loopes gjennom nodene eller treet vårt med en while løkke der det sjekkes foreldre, rot og
 barn og verdiene blir sammenlignet og da flyttes. videre lager vi ny node for verdien og går gjennom tre if tester som
 sikrer noden. om alt er ok så antallet økes og boolean returnerer true.
 
-I oppgave 2 så brukte jeg en ... til å ...
+I oppgave 2 brukte jeg while løkke,compare til å sammenligne verdier om de ikke er i rot
+og de blei gjennom en del if tester som klargjør ting om det skjer det og det.
+for antall skulle bare returneres antall siden denne funksjonen trengte ikke noe mer.
+sammen skjer på tøm og da satt jeg antall lik null og hvis det er slik så er det tøm.
+
+I oppgave 3 samme prinsip while løøker brukes og loppes gjennom tret og det innkommer en del if tester
+som sikrer oss tilfeller eller at det nærmere oss ønskende situasjon. ting har blitt kommentert og ser
+faktisk bra.
+
+I oppgave 4 brukes første postorden og nest postorden og thats it. det blir tatt rekursivt ut i en andre
+funksjonen. fuksjonen tar inn oppgave og hvilken siden dette skal skje og til slutt skrives ut.
+
+I oppgave 5 ble brukt eksemplet vi fikk i obligen for å omgjøre binærtreet til array. ved hjelp av
+new SBinTre<>(compaartor.naturalOrder()) laget jeg nytt tre og tok verdier ut av med queue funksjonen.
+videre blei de brukt i en array listet og ble limt inn.
+
+I oppgave 6 brukte jeg eksemplet og boken som kilde. kopierte koder fra programkode 5.2 8 d). brukte mangevis
+if tester som faktisk øddelegger farten for å avklare tre tilfeller som er mulig. det som var nytt
+enn opppgave 5 var at jeg brukte en for løkke som innholder int verdi : a og leggger verdien inn i treet.
+
--- oblig-3-template-master/src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ oblig-3-template-master/src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -1,10 +1,7 @@
 package no.oslomet.cs.algdat.Oblig3;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class SBinTre<T> {
     private static final class Node<T>   // en indre nodeklasse
@@ -108,11 +105,64 @@ public class SBinTre<T> {
     }
 
     public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(verdi == null){
+            return false;
+        }
+
+        Node<T> p = rot, q = null; // q skal være forelder til p
+        while (p != null){
+            int komp = comp.compare(verdi, p.verdi); //sammenligner
+            if (komp < 0){//hvis så går vi venstre
+                q = p;
+                p = p.venstre;
+            }else if(komp > 0){//hvis så, går vi til høyre
+                q = p;
+                p = p.høyre;
+            }
+            else break; //finner ikke verdie så settes på break
+        }
+        if (p == null) return false; //om den verdien var null
+        if (p.venstre == null || p.høyre == null) {
+            Node<T> barn = p.venstre != null ? p.venstre : p.høyre;
+            if (p == rot) {
+                rot = barn;
+            } else if (p == q.venstre) {
+                q.venstre = barn;
+            } else q.høyre = barn;
+
+            if (barn != null) {
+                barn.forelder = q;
+            }
+        }else{
+                Node<T> s = p, r =p.høyre; //finner neste
+                while (r.venstre != null){
+                    s = r;// s er forelder til r
+                    r = r.venstre;
+                }
+
+                p.verdi = r.verdi;
+                if(s != p){
+                    s.venstre = r.høyre;
+                } else {
+                    s.høyre = r.høyre;
+                }
+                if (r. høyre != null){
+                    r.høyre.forelder = s;
+                }
+            }
+        antall--;
+        return true;
     }
 
     public int fjernAlle(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (tom()){
+            return 0;
+        }
+        int fjernetAntall = 0;
+        while (fjern(verdi)){
+            fjernetAntall++;
+        }
+        return fjernetAntall;
     }
 
     public int antall(T verdi) {
@@ -132,7 +182,27 @@ public class SBinTre<T> {
     }
 
     public void nullstill() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (antall == 0){//returneres ingenenting om antallet er null
+            return;
+        }
+
+        Node<T> p = rot;
+
+        int stops = antall;
+        p = førstePostorden(p);
+
+        while (stops != 0){ // der hvor antallet ikke er null så skjer iterasjon som fjerner
+            if(p != null){
+                fjern(p.verdi);
+            }
+            if (p != null){
+                p.verdi = null;
+            }
+            if (p != null){
+                p = nestePostorden(p);//bruker denne funksjonen for å gå videre til neste
+            }
+            stops--;
+        }
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
@@ -164,7 +234,14 @@ public class SBinTre<T> {
     }
 
     public void postorden(Oppgave<? super T> oppgave) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Node<T> p = rot;
+
+        Node<T> fors = førstePostorden(p);//finner første noden
+        oppgave.utførOppgave(fors.verdi);
+        while (fors.forelder != null){
+            fors = nestePostorden(fors); //forste/fors blir oppdatert
+            oppgave.utførOppgave(Objects.requireNonNull(fors).verdi);
+        }
     }
 
     public void postordenRecursive(Oppgave<? super T> oppgave) {
@@ -172,15 +249,42 @@ public class SBinTre<T> {
     }
 
     private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(p == null){
+            return;
+        }
+
+        postordenRecursive(p.venstre, oppgave);//tar kun venstrebarn av p
+        postordenRecursive(p.høyre, oppgave);// tar kun høyrebarn av p
+
+        oppgave.utførOppgave(p.verdi);//utfører
     }
 
     public ArrayList<T> serialize() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        ArrayList<T> nivatre = new ArrayList<>();//Initialiserer Arrayet
+        Queue<Node<T>> queue = new LinkedList<>();//kø
+
+        queue.add(rot);//legger roten som første element
+        while (!queue.isEmpty()){//while treet ikke er tøm så
+            Node<T> p = queue.remove();//fjernen verdien i queue fra treet
+            nivatre.add(p.verdi); // legger inn øverste verdien i arrayet
+
+            if (p.venstre != null){
+                queue.add(p.venstre); // venstrebarn legges i køen
+            }
+            if (p.høyre != null){
+                queue.add(p.høyre); // høyrebarn legges i køen
+            }
+        }
+        return nivatre;
     }
 
     static <K> SBinTre<K> deserialize(ArrayList<K> data, Comparator<? super K> c) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        SBinTre<K> tre = new SBinTre<>(c);
+
+        for (K verdi : data){
+            tre.leggInn(verdi);
+        }
+        return tre;
     }
 
 
